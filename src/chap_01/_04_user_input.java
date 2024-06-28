e782792eff19abac1c5a85f6883957ad349825da
jinukee <jinukee333@gmail.com>
2024-06-28 14:48:13 +0900

    learned how to use user input

---
+++ src/chap_01/_04_user_input.java
@@ -0,0 +1,24 @@
+package chap_01;
+
+import java.util.Scanner;
+
+public class _04_user_input {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine(); // nextInt() method can't skip \n value
+        System.out.println("What is your favorite food?");
+        String food = scanner.nextLine();
+
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like " + food);
+
+    }
+}

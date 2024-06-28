7b39ccc9c846642d3ceaa6861253725c4fe65e68
jinukee <jinukee333@gmail.com>
2024-06-28 15:11:44 +0900

    learned expressions

---
+++ src/chap_01/_05_expressions.java
@@ -0,0 +1,21 @@
+package chap_01;
+
+public class _05_expressions {
+    public static void main(String[] args) {
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = + - * / %
+
+        int friends = 10;
+
+        friends++;
+        friends--;
+        friends *= 2;
+
+        double n = 10;
+        n = (double) n / 3;
+
+        System.out.println(friends);
+        System.out.println(n);
+    }
+}

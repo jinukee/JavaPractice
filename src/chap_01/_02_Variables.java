26f3375ae2e228bdcead7c196bb8d0680c3f8aa0
jinukee <jinukee333@gmail.com>
2024-06-28 14:18:27 +0900

    variable study 추가

---
+++ src/chap_01/_02_Variables.java
@@ -0,0 +1,18 @@
+package chap_01;
+
+public class _02_Variables {
+    public static void main(String[] args) {
+
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '@';
+        String name = "jinukee";
+
+        System.out.println("Hello " + name);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(symbol);
+    }
+}
